6b3784a84fa6c795574ef2c1f7b83f7342efb94f
Sasha_tshk <alexander.tashkinov123@gmail.com>
2025-06-16 13:56:27 +0100

    Update SaleDTO.java

--- seminarie3/src/model/SaleDTO.java
+++ seminarie3/src/model/SaleDTO.java
@@ -4,21 +4,28 @@ package model;
 import java.util.List;
 
 /**
- * DTO med försäljningsdata.
+ * Data Transfer Object (DTO) för att transportera information om en försäljning.
  */
 public class SaleDTO {
-    private List<SoldItemDTO> items;
+    private List<SoldItemDTO> soldItems;
     private double totalPrice;
     private boolean discountApplied;
 
-    public SaleDTO(List<SoldItemDTO> items, double totalPrice, boolean discountApplied) {
-        this.items = items;
+    /**
+     * Skapar en SaleDTO med information om försäljningen.
+     *
+     * @param soldItems Sålda artiklar i försäljningen.
+     * @param totalPrice Totalpriset för försäljningen.
+     * @param discountApplied Om rabatt har tillämpats.
+     */
+    public SaleDTO(List<SoldItemDTO> soldItems, double totalPrice, boolean discountApplied) {
+        this.soldItems = soldItems;
         this.totalPrice = totalPrice;
         this.discountApplied = discountApplied;
     }
 
-    public List<SoldItemDTO> getItems() {
-        return items;
+    public List<SoldItemDTO> getSoldItems() {
+        return soldItems;
     }
 
     public double getTotalPrice() {
