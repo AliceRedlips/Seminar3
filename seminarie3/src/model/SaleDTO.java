3492f40d9349a82026a955b1b31be42715e90970
Sasha_tshk <alexander.tashkinov123@gmail.com>
2025-06-16 14:14:39 +0100

    Update SaleDTO.java

--- seminarie3/src/model/SaleDTO.java
+++ seminarie3/src/model/SaleDTO.java
@@ -1,27 +1,42 @@
 
 package model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
- * Data Transfer Object (DTO) för att transportera information om en försäljning.
+ * DTO för att transportera information om en försäljning.
  */
 public class SaleDTO {
-    private List<SoldItemDTO> soldItems;
-    private double totalPrice;
-    private boolean discountApplied;
+    private final List<SoldItemDTO> soldItems;
+    private final double totalPrice;
+    private final boolean discountApplied;
 
     /**
      * Skapar en SaleDTO med information om försäljningen.
      *
-     * @param soldItems Sålda artiklar i försäljningen.
+     * @param soldItems Artiklar i försäljningen som SoldItemDTO.
      * @param totalPrice Totalpriset för försäljningen.
      * @param discountApplied Om rabatt har tillämpats.
      */
-    public SaleDTO(List<SoldItemDTO> soldItems, double totalPrice, boolean discountApplied) {
-        this.soldItems = soldItems;
+    public SaleDTO(List<SoldItem> soldItems, double totalPrice, boolean discountApplied) {
+        this.soldItems = convertToDTOs(soldItems);
         this.totalPrice = totalPrice;
         this.discountApplied = discountApplied;
+    }
+
+    private List<SoldItemDTO> convertToDTOs(List<SoldItem> soldItems) {
+        List<SoldItemDTO> dtos = new ArrayList<>();
+        for (SoldItem sold : soldItems) {
+            dtos.add(new SoldItemDTO(
+                    sold.getItemID(),
+                    sold.getName(),
+                    sold.getPrice(),
+                    sold.getVAT(),
+                    sold.getQuantity()
+            ));
+        }
+        return dtos;
     }
 
     public List<SoldItemDTO> getSoldItems() {
